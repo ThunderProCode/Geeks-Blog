805fe79fd5b4edc380dbd18c2b229b2f98af6579
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-07-27 15:34:29 -0600

    firebase first deploy

--- dist/src/firebase.js
+++ dist/src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { firebaseConfig } from './firebaseConfig';
+import { firebaseConfig } from '../firebaseConfig';
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
